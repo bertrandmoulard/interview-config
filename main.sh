35cb859491023733356b38e8ab7cff99c29c8a15
Bertrand Moulard <bmoulard@etsy.com>
2016-03-07 14:58:07 -0500

    Fix bugs in main script

--- main.sh
+++ main.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 CURRENT_DIR=`pwd`
-sudo cat 000-default.conf > /etc/apache2/sites-available/000-default.conf
+sudo cp 000-default.conf /etc/apache2/sites-available/
+sudo ln -s /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/
 
 mysql -uroot -e "CREATE USER 'cooking'@'localhost' IDENTIFIED BY 'cooking';"
+mysql -uroot -e "CREATE DATABASE cooking"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'cooking'@'localhost' WITH GRANT OPTION"
 
-git clone https://github.com/bertrandmoulard/cooking.git /var/www/
-cd cooking
-cat config.ini > /var/www/cooking/config.ini
+cd /var/www/
+git clone https://github.com/bertrandmoulard/cooking.git
+cat $CURRENT_DIR/config.ini > /var/www/cooking/config.ini
 cd /var/www/cooking
 wget https://getcomposer.org/composer.phar
 php composer.phar install
+cd db
+mysql -ucooking -pcooking cooking < schema.sql
 sudo service nginx stop
-sudo service apache restart
+sudo service apache2 restart
